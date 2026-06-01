2d58480321a20750673887239825154110d0857a
Hannah Santos <hannahsantos@usf.edu>
2026-06-01 01:20:38 -0400

    Completed Coding Challenge 003

--- Coding Challenges/Coding Challenge 3/Customer-Tracker/cc_3.js
+++ Coding Challenges/Coding Challenge 3/Customer-Tracker/cc_3.js
@@ -1,3 +1,33 @@
-let customer = [ name
-    
-]
+let customers = [
+    {
+        name: "Johnny Rose",
+        email: "johnny.rose@example.com",
+        purchases: ["Suit", "Briefcase"]
+    },
+    {
+        name: "Moira Rose",
+        email: "moira.rose@example.com",
+        purchases: ["Dresses", "Hats"]
+    },
+    {
+        name: "David Rose",
+        email: "david.rose@example.com",
+        purchases: ["Sweater", "Sunglasses"]
+    }
+];
+
+customers.push({
+    name: "Alexis Rose",
+    email: "alexis.rose@example.com",
+    purchases: ["Handbags", "Shoes"]
+});
+
+customers.shift();
+customers[0].email = "moira.rose01@example.com";
+customers[1].purchases.push("Skincare Products");
+
+customers.forEach(function(customer) {
+    console.log(`Name: ${customer.name}`);
+    console.log(`Email: ${customer.email}`);
+    console.log(`Total Purchases: ${customer.purchases.length}`);
+});
